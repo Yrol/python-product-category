59e8abc0555355d6e950fa8b047012ce85e2c93e
Yrol <yrollgayanth@yahoo.com>
2021-12-10 15:02:45 +1100

    Adding the product relationship

--- app/category/serializers.py
+++ app/category/serializers.py
@@ -1,8 +1,9 @@
 from rest_framework import serializers
 from core.models import Category
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    products = serializers.HyperlinkedRelatedField(many=True, read_only=True, view_name='product:product-detail')
     class Meta:
         model = Category
-        fields = ['id', 'url', 'name', 'description']
+        fields = ['id', 'url', 'name', 'description', 'products']
         read_only_fields = ('id',)
 
--- app/core/models.py
+++ app/core/models.py
@@ -5,6 +5,7 @@ from datetime import datetime
 # Create your models here.
 class Category(models.Model):
 
+    id =  models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
 
@@ -14,11 +15,12 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    id = models.IntegerField(primary_key=True)
     name = models.CharField(max_length=255)
     description = models.TextField(blank=True, null=True)
     manufacturing_date = models.DateField(default=datetime.now)
     price = models.CharField(max_length=255)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
 
     def __str__(self):
         return '%s %s %s' % (self.name, self.description,
