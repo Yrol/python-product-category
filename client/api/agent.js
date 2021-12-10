2aac932d79008308951d2a6439a44674880b6597
Yrol <yrollgayanth@yahoo.com>
2021-12-10 18:55:52 +1100

    Adding the button component and creating the homepage

---
+++ client/api/agent.js
@@ -0,0 +1,28 @@
+import axios, { AxiosResponse } from 'axios'
+
+axios.defaults.baseURL = '/api';
+
+const responseBody = (response) => response.data;
+
+const requests = {
+  get: (url) => axios.get(url).then(responseBody),
+  post: (url, body) => axios.post(url, body).then(responseBody),
+  put: (url, body) => axios.put(url, body).then(responseBody),
+  del: (url) => axios.delete(url).then(responseBody)
+}
+
+const Products = {
+  all: () => requests.get(`/product`),
+  details: (id) => requests.get(`/product/${id}`),
+  create: (post) => requests.post('/product', post),
+  update: (id) => requests.put(`/product/${id}`, post),
+  delete: (id) => requests.del(`/product/${id}`),
+}
+
+const Category = {
+  all: () => requests.get(`/category`),
+  details: (id) => requests.get(`/category/${id}`),
+  create: (post) => requests.post('/category', post),
+  update: (id) => requests.put(`/category/${id}`, post),
+  delete: (id) => requests.del(`/category/${id}`),
+}
--- client/pages/index.vue
+++ client/pages/index.vue
@@ -1,7 +1,47 @@
 <template>
-  <Tutorial/>
+  <client-only>
+    <div>
+      <div class="container mx-auto p-2">
+        <div
+          class="max-w-sm mx-auto my-24 bg-white px-5 py-10 rounded shadow-xl"
+        >
+          <div class="text-center mb-8">
+            <h1 class="font-bold text-2xl font-bold">Options</h1>
+          </div>
+          <div class="mt-10">
+            <Button
+              :variant="'success'"
+              :loading="false"
+              :disableButton="false"
+              icon="sign-in-alt"
+              size="small"
+              width="full"
+            >
+              Products
+            </Button>
+          </div>
+          <div class="mt-10">
+            <Button
+              :variant="'primary'"
+              :loading="false"
+              :disableButton="false"
+              icon="sign-in-alt"
+              size="small"
+              width="full"
+            >
+              Categories
+            </Button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </client-only>
 </template>
-
 <script>
-export default {}
+import Button from "~/components/Button";
+export default {
+  head: {
+    title: "Python App",
+  },
+};
 </script>
